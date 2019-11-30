c65a477c67ac9a6d2b35be606d14aa6f9d06fc01
Liene <rudukaliene@gmail.com>
2019-11-30 15:42:17 +0200

    myproject

--- MY_PROJECT/src/com/accenture/HPApp/Muggles.java
+++ MY_PROJECT/src/com/accenture/HPApp/Muggles.java
@@ -1,5 +1,59 @@
 package com.accenture.HPApp;
 
 public class Muggles {
+	private String name;
+	private String location;
+	private int health;
+	private boolean isWizard;
 
+	public Muggles(String name, String location, int health, boolean isWizard) {
+		this.name = name;
+		this.location = location;
+		this.health = health;
+		this.isWizard = isWizard;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public boolean isWizard() {
+		return true;
+	}
+
+	public void summon(Muggles muggle) {
+
+	}
+
+	private int makeDamage() {
+		return 10;
+	}
+
+	public int restoreHealth(int health) {
+		return 15;
+	}
+
+	public void fight(Muggles muggle) {
+
+	}
+
+	public String invitieTo(Muggles muggle, String location) {
+		return "school";
+	}
+
+	public String info() {
+		return "";
+	}
 }
