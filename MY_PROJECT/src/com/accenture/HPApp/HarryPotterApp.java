f38d26969839b9222d9c171e9cf41b31d0221eeb
Liene <rudukaliene@gmail.com>
2019-12-04 21:16:45 +0200

    project

--- MY_PROJECT/src/com/accenture/HPApp/HarryPotterApp.java
+++ MY_PROJECT/src/com/accenture/HPApp/HarryPotterApp.java
@@ -2,35 +2,35 @@ package com.accenture.HPApp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HarryPotterApp {
 	static List<Object> allCharacters = new ArrayList<Object>();
 
 	public static void main(String[] args) {
-		Wizards harry = new Wizards("Harry Potter", "school", 34, 15, true);
-		Wizards drako = new Wizards("Drako Malfoy", "diagonally", 77, 12, true);
+		Wizards harry = new Wizards("Harry Potter", "Hogwarts", 34, 15, true);
+		Wizards drako = new Wizards("Drako Malfoy", "Diagon Ally", 77, 12, true);
 		Muggles janka = new Muggles("Janka", "Plavnieki", 77, 19, false);
 
 		allCharacters.add(harry);
 		allCharacters.add(drako);
 		allCharacters.add(janka);
+//		Menu();
+		// janka.fight(drako);
 
-		janka.fight(drako);
-		harry.fight(janka);
-		drako.fight(harry);
-		// Menu();//
-		harry.info();
-		// drako.info();
-		// janka.info();
-		//
-		// harry.goTo(drako);
-		// harry.goTo(drako);
-		// drako.summon(janka);
-		//
+		// drako.fight(harry);
+
 		// harry.info();
 		// drako.info();
 		// janka.info();
+		// harry.goTo(drako);
+		// drako.summon(janka);
+//		harry.fight(janka);
+		System.out.println(muggleWeapons());
+
+		// harry.fight(drako);
+
 	}
 
 	public static void Menu() {
@@ -85,46 +85,78 @@ public class HarryPotterApp {
 	public static void seeOptions() {
 		System.out.println("What would you like to do?");
 	}
-	// for (int i = 0; i<list.size(); i++) {
-		// if (list.get(i) instanceof Wizards) {
-	// Wizards wizard = (Wizards) list.get(i);
-	// System.out.println("Press " + i + " for " + wizard.getName());
-	// } else {
-	// Muggles muggle = (Muggles) list.get(i);
-	// System.out.println("Press " + i + " for " + muggle.getName());
-	// }
-	// public static Muggles createByUser() {
-	// Scanner scanner = new Scanner(System.in);
-	// System.out
-	// .println("You have chosen to create a new character of Harry Potter App!");
-	// System.out.println("Please, enter the name of the character: ");
-	// String inputName = scanner.nextLine();
-	// System.out.println("Please, enter the tower: ");
-	// int inputTower = scanner.nextInt();
-	// System.out.println("Enter the location " + inputName
-	// + " will be created in: ");
-	// String inputLocation = scanner.nextLine();
-	// System.out
-	// .println("Is "
-	// + inputName
-	// +
-	// " going to be a wizard or muggle? (type 'w' for wizard and 'm' for muggle)");
-	// String inputIsWizard = "";
-	// while (!(inputIsWizard == "w" || inputIsWizard == "m")) {
-	// if (!(inputIsWizard == "")) {
-	// System.out
-	// .println("Your wish is not clear! Please, tell, if "
-	// + inputName
-	// +
-	// " is going to be a wizard or muggle? (type 'w' for wizard and 'm' for muggle)");
-	// }
-	// inputIsWizard = scanner.nextLine();
-	// }
-	// boolean isWizard = inputIsWizard == "w" ? true : false;
-	// scanner.close();
-	// Muggles creation = new Muggles(inputName, inputTower, inputLocation,
-	// isWizard);
-	// return creation;}
 
-}
+	public static String[] optionList() {
+		String[] options = new String[] { "Rename character",
+				"Move character to a different location",
+				"Summon another character to this characters location",
+				"Invite another character to go to a new location together",
+				"See where the character spent the schooltime at",
+				"Fight another character", "See full info of the character" };
+		return options;
+	}
 
+	public static String muggleWeapons() {
+		String[] options = new String[] {"with a baseball bat.",
+				" with a fist.", " with a right hook.", " with a left hook.",
+				" with a Molotov coctail.", " with a pocket knife.",
+				" with a Japanese sword.", " with an A-91." };
+		Random r = new Random();
+		String printout = "";
+		int random = r.nextInt((options.length -0) + 1) +0;
+		for (int i = 0; i < options.length; i++) {
+			if(i == random) 
+			printout = options[i];
+		}
+		return printout;
+	}
+
+//	public static String wizardWeapons() {
+//		String[] options = new String[] { " with a nasty spell.",
+//				" with a jelly-fingers curse.", " with a jelly-legs Jinx.",
+//				" with a leg-locker curse.", " with a pimple jinx.",
+//				" with a stinging hex.", " with a tickling charm.",
+//				" with muggle-repelling charm." };
+////		return options;
+	}
+//}
+
+// public static Muggles createByUser() {
+//
+// Scanner scanner = new Scanner(System.in);
+// System.out
+// .println("You have chosen to create a new character of Harry Potter App!");
+// System.out.println("Please, enter the name of the character: ");
+//
+// String inputName = scanner.nextLine();
+// System.out.println("Please, enter the tower: ");
+// int inputTower = scanner.nextInt();
+// System.out.println("Enter the location " + inputName
+// + " will be created in: ");
+// String inputLocation = scanner.nextLine();
+// System.out
+// .println("Is "
+// + inputName
+// +
+// " going to be a wizard or muggle? (type 'w' for wizard and 'm' for muggle)");
+// String inputIsWizard = "";
+// while (!(inputIsWizard == "w" || inputIsWizard == "m")) {
+// if (!(inputIsWizard == "")) {
+// System.out
+// .println("Your wish is not clear! Please, tell, if "
+// + inputName
+// +
+// " is going to be a wizard or muggle? (type 'w' for wizard and 'm' for muggle)");
+// }
+// inputIsWizard = scanner.nextLine();
+// }
+// boolean isWizard = inputIsWizard == "w" ? true : false;
+// scanner.close();
+//
+// Muggles creation = new Muggles(inputName, inputTower, inputLocation,
+// isWizard);
+// return creation;
+//
+// }
+//
+// }
